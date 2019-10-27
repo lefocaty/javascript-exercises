8f8ce33f44296b739b43fa66399edb9cae3714c7
Lefocaty <lefocatynyangweso@gmail.com>
2019-10-27 22:11:58 +0300

    another commit

--- 1-basics/basics.js
+++ 1-basics/basics.js
@@ -215,7 +215,7 @@ object.someProperty = undefined;
             ////////////////////////////////////////////////////////////////////
             // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
             // ✏️ ADD CODE HERE ✏️
-  
+  const secondArray = Array.of(...firstArray);
             ////////////////////////////////////////////////////////////////////
 
             expect(firstArray).to.eql([1, 2, 3]);
